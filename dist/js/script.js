4ef4cf61417da41b8ce75a3909eed84283864a0e
Mihail <liahim.veahcen@gmail.com>
2023-11-21 16:24:48 +0300

    fix footer and made reviews

--- dist/js/script.js
+++ dist/js/script.js
@@ -21,4 +21,28 @@ $(document).ready(function(){
     prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
     nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
   });
+
+  $('.reviews__slider-carousel').slick({
+    centerMode: true,
+    variableWidth: true,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ],
+    prevArrow: '.slick-prev2',
+    nextArrow: '.slick-next2',
+  });
 });
