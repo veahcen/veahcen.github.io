bda7003da00c9ceb5af18902525d73f55ded6871
Mihail <liahim.veahcen@gmail.com>
2023-12-01 19:10:28 +0300

    some fix

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,27 +1,27 @@
 $(document).ready(function(){
-  // $('.slider__carousel').slick({
-  //   variableWidth: true,
-  //   centerMode: false,
-  //   slidesToShow: 3,
-  //   responsive: [
-  //     {
-  //       breakpoint: 768,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 2
-  //       }
-  //     },
-  //     {
-  //       breakpoint: 480,
-  //       settings: {
-  //         arrows: false,
-  //         slidesToShow: 1
-  //       }
-  //     }
-  //   ],
-  //   prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
-  //   nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
-  // });
+  $('.slider__carousel').slick({
+    variableWidth: true,
+    centerMode: false,
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          slidesToShow: 1
+        }
+      }
+    ],
+    prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+  });
 
   $('.reviews__slider-carousel').slick({
     swipe: false,
